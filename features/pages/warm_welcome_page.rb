1b0b4a6d3af79e75a1fbaee9933c8e815d44501e
V.Bober <boberv@gmail.com>
2014-10-03 01:45:01 +0300

    Extending the page.

--- features/pages/warm_welcome_page.rb
+++ features/pages/warm_welcome_page.rb
@@ -1,31 +1,50 @@
 require_relative 'page_object'
 
 class WarmWelcomePage < Unshred::Tag::PageObject
-
-  @elements = {}
-  @elements[:facebook_button] = ''
-  @elements[:twitter_button] = ''
-  @elements[:gmail_button] = ''
-  @elements[:vkontakte_button] = ''
+  def initialize(a_world)
+    self.world = a_world
+    @elements = {}
+    @elements[:sign_in_set] = "//ul[@id='signin']"
+    @elements[:facebook_button] = "//a[@class='facebook']"
+    @elements[:twitter_button] = "//a[@class='twitter']"
+    @elements[:gmail_button] = "//a[@class='googleplus']"
+    @elements[:vkontakte_button] = "//a[@class='vkontakte']"
+  end
 
   def trait
     "//div[@id='warm-welcome']"
   end
 
+  def facebook_signin_option?
+    page.has_xpath? @elements[:facebook_button]
+  end
+
+  def twitter_signin_option?
+    page.has_xpath? @elements[:twitter_button]
+  end
+
+  def gmail_signin_option?
+    page.has_xpath? @elements[:gmail_button]
+  end
+
+  def vkontakte_signin_option?
+    page.has_xpath? @elements[:vkontakte_button]
+  end
+
   def click_facebook_sign_in
-    page.click_link  @elements[:facebook_button]
+    page.click_link @elements[:facebook_button]
   end
 
   def click_twitter_sign_in
-    page.click_link  @elements[:twitter_button]
+    page.click_link @elements[:twitter_button]
   end
 
   def click_gmail_sign_in
-    page.click_link  @elements[:gmail_button]
+    page.click_link @elements[:gmail_button]
   end
 
   def click_vkontakte_sign_in
-    page.click_link  @elements[:vkontakte_button]
+    page.click_link @elements[:vkontakte_button]
   end
 
 end
